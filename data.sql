b232cae5d25798def3586b356bf4f33e472dca67
Santiago <alexander.cardenas10@outlook.com>
2022-07-04 17:33:25 -0500

    Add queries to queries.sql

--- data.sql
+++ data.sql
@@ -12,5 +12,20 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom', '1998-10-13', 3, true, 17);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Ditto', '2022-05-14', 4, true, 22);
 
+BEGIN;
+UPDATE animals SET species = 'undefined';
+ROLLBACK;
 
+BEGIN;
+UPDATE animals SET species='digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species='pokemon' WHERE species IS NULL;
+COMMIT;
+
+BEGIN;
+DELETE FROM ANIMALS WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg=weight_kg*(-1);
+ROLLBACK TO SP1;
+UPDATE animals SET weight_kg=weight_kg*(-1) WHERE weight_kg <0;
+COMMIT;
 
