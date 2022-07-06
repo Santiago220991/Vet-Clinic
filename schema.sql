acc634b2ee11eb62fea1b1012b9685073f0fe6a7
Santiago <alexander.cardenas10@outlook.com>
2022-07-06 10:40:51 -0500

    Insert data to vets table

--- schema.sql
+++ schema.sql
@@ -31,4 +31,10 @@ ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species (id);
 ALTER TABLE animals ADD owner_id INT;
 ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners (id);
 
-
+CREATE TABLE vets (id INT GENERATED ALWAYS AS IDENTITY, name TEXT, age INT, date_of_graduation DATE, PRIMARY KEY(id));
+CREATE TABLE specializations (species_id INT, vets_id INT);
+ALTER TABLE specializations ADD FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE specializations ADD FOREIGN KEY (vets_id) REFERENCES vets (id);
+CREATE TABLE visits (name_id INT, vets_id INT);
+ALTER TABLE visits ADD FOREIGN KEY (name_id) REFERENCES animals (id); 
+ALTER TABLE visits ADD FOREIGN KEY (vets_id) REFERENCES vets (id);
